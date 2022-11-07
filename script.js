57c819f0f23dcae6ba2661bd3de837a42f78ed64
SkinnV1 <thatcisio77@gmail.com>
2022-11-07 09:55:08 -0300

    final do projeto

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ function createGame(t1, hour, t2){
     `
 }
 
+let delay = -0.4;
 function createCard(date, day, games) {
+    delay = delay + 0.4;
     return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
             <h2>${date}<span>${day}</span></h2>
             <ul>
                ${games}
@@ -20,20 +22,11 @@ function createCard(date, day, games) {
     `
 }
 
-document.querySelector(`#app`).innerHTML =
-`
-    <header>
-        <img src="./assets/logo.svg" alt="Calendário"> 
-    </header>
-    <main id="cards">
-        ${createCard('24/11', 'Quinta',
-            createGame('suiça', '07:00', 'camarões') + createGame('brasil', '16:00', 'servia')
-        )}
-        ${createCard('28/11', 'Segunda',
-            createGame('camarões', '07:00', 'servia') + createGame('brasil', '13:00', 'suiça')
-        )}
-        ${createCard('02/12', 'Sexta',
-            createGame('brasil', '16:00', 'camarões')
-        )}
-    </main>
-`
+document.querySelector("#cards").innerHTML =
+
+    createCard('24/11', 'Quinta', 
+    createGame('suiça', '07:00', 'camarões') + createGame('brasil', '16:00', 'servia')) +
+    createCard('28/11', 'Segunda',
+     createGame('camarões', '07:00', 'servia') + createGame('brasil', '13:00', 'suiça')) +
+    createCard('02/12', 'Sexta', 
+    createGame('brasil', '16:00', 'camarões')) 
